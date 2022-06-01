b874d4bdebdf49957e343d090108336db9abbd03
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-06-01 11:03:50 +0500

    service

---
+++ src/components/services-card/services-card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Description, Row, Title, TitleWrapper, Wrapper } from "./servive-csrd.e";
+
+const ServicesCard = () => {
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        <Description>CATEGORY</Description>
+        <Title>We Offer Best Services</Title>
+      </TitleWrapper>
+      <Row></Row>
+    </Wrapper>
+  );
+};
+
+export default ServicesCard;
---
+++ src/components/services-card/servive-csrd.e.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    
+`
+export const TitleWrapper = styled.div`
+    
+`
+
+export const Description = styled.p`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  color: #5e6282;
+`;
+
+export const Title  = styled.h1`
+    
+`
+
+
+export const Row = styled.div`
+    
+`
