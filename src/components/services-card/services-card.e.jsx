d0f9925a5f32d5ef3ef723534cd46e795566e421
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-06-12 22:44:04 +0500

    services-card finished

--- src/components/services-card/services-card.e.jsx
+++ src/components/services-card/services-card.e.jsx
@@ -1,15 +1,22 @@
-import styled,{keyframes} from "styled-components";
-import plus from "../../assets/img/service-card/Group 4.svg";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+ overflow: hidden;
+`;
 export const TitleWrapper = styled.div`
   position: relative;
  .plus {
+  z-index: -1;
     position: absolute;
     top: 0;
     right: 0;
     width: 153px;
     height: 166px;
+    @media screen and (max-width: 930px) {
+      width: 90px;
+      height: auto;
+    }
   }
 `;
 
@@ -35,41 +42,91 @@ export const Title = styled.h1`
   text-transform: capitalize;
   color: #14183e;
   margin-bottom: 68px;
+  @media screen and (max-width: 930px) {
+    font-size: 35px;
+    line-height: 44px;
+  }
 `;
 
-export const Row = styled.div`
-margin: 0 100px;
-padding-bottom:106px ;
+export const Row = styled(motion.div)`
+  padding-bottom: 106px;
+  margin: 0 61px;
+  cursor: grab;
+  @media screen and (max-width: 930px) {
+    margin: 0 30px;
+  }
+  @media screen and (max-width: 600px) {
+    /* width: 100%; */
+    width: calc(100% - 60px);
+  }
 `;
 
-export const CardWrapper = styled.div`
-width: 100%;
+export const CardWrapper = styled(motion.div)`
+  width: 100%;
   display: grid;
-  grid-template-columns: repeat(4,1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 50px;
-`
+  @media screen and (max-width: 1250px) {
+    grid-gap: 30px;
+  }
+  @media screen and (max-width: 1000px) {
+    padding: 30px 0;
+    overflow-y: hidden;
+    display: flex;
+  }
+`;
 
-export const Card = styled.div`
+export const Card = styled(motion.div)`
   /* width: 267px; */
   height: 314px;
   background: #ffffff;
-  box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.02),
-    0px 64.8148px 46.8519px rgba(0, 0, 0, 0.0151852),
-    0px 38.5185px 25.4815px rgba(0, 0, 0, 0.0121481),
-    0px 20px 13px rgba(0, 0, 0, 0.01),
-    0px 8.14815px 6.51852px rgba(0, 0, 0, 0.00785185),
-    0px 1.85185px 3.14815px rgba(0, 0, 0, 0.00481481);
   border-radius: 36px;
   padding: 40px 32px;
   box-sizing: border-box;
+  transition: all linear 0.225s;
+  border: 1px solid #faa307;
+  pointer-events: none;
   &:hover {
     .img1 {
       transform: scale(1.2);
     }
+    box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.02),
+      0px 64.8148px 46.8519px rgba(0, 0, 0, 0.0151852),
+      0px 38.5185px 25.4815px rgba(0, 0, 0, 0.0121481),
+      0px 20px 13px rgba(0, 0, 0, 0.01),
+      0px 8.14815px 6.51852px rgba(0, 0, 0, 0.00785185),
+      0px 1.85185px 3.14815px rgba(0, 0, 0, 0.00481481);
   }
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  position: relative;
+  &:nth-child(2) {
+    .img_2 {
+      position: absolute;
+      z-index: -1;
+      left: -35px;
+      bottom: -35px;
+      width: 100px;
+      height: 100px;
+    }
+  }
+  @media screen and (max-width: 1250px) {
+    padding: 30px 15px;
+  }
+  @media screen and (max-width: 1000px) {
+    min-width: 267px;
+    &:nth-child(2) {
+      .img_2 {
+        position: absolute;
+        z-index: -1;
+        left: -12px;
+        bottom: -20px;
+        width: 100px;
+        height: 100px;
+      }
+    }
+  }
 `;
 
 export const CardBody = styled.div`
--- src/components/services-card/services-card.jsx
+++ src/components/services-card/services-card.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import {
   Card,
   CardBody,
@@ -15,8 +16,15 @@ import antena from '../../assets/img/service-card/Group 48.svg'
 import plane from '../../assets/img/service-card/Group 51 (1).svg'
 import mikrofon from "../../assets/img/service-card/Group 50.svg";
 import settings from '../../assets/img/service-card/Group 49.svg'
+import images from '../../assets/img/service-card/Rectangle 157.svg'
 
 const ServicesCard = () => {
+   const [width, setWidth] = useState(0);
+   const carousel = useRef();
+
+   useEffect(() => {
+     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+   }, []);
   return (
     <Wrapper>
       <TitleWrapper plus={plus}>
@@ -26,8 +34,12 @@ const ServicesCard = () => {
           <img src={plus} alt="img not found" />
         </div>
       </TitleWrapper>
-      <Row>
-        <CardWrapper>
+      <Row
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <CardWrapper drag="x" dragConstraints={{ right: 0, left: -width }}>
           <Card>
             <CardBody>
               <img src={antena} className="img1" alt="img not found" />
@@ -49,6 +61,7 @@ const ServicesCard = () => {
                 Built Wicket longer admire do barton vanity itself do in it.
               </p>
             </CardFooter>
+            <img src={images} className="img_2" alt="img not found" />
           </Card>
           <Card>
             <CardBody>
